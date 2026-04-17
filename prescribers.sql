07e5279b3c22288f01ec8c986f50ca191ad13f3b
colton kamenca <coltonkamenca@gmail.com>
2026-04-16 21:20:09 -0500

    Not much progress, mostly just looking through the data

---
+++ prescribers.sql
@@ -0,0 +1,13 @@
+1. 
+    a. Which prescriber had the highest total number of claims (totaled over all drugs)? Report the npi and the total number of claims.
+    
+    b. Repeat the above, but this time report the nppes_provider_first_name, nppes_provider_last_org_name,  specialty_description, and the total number of claims.
+
+2. 
+    a. Which specialty had the most total number of claims (totaled over all drugs)?
+
+    b. Which specialty had the most total number of claims for opioids?
+
+    c. **Challenge Question:** Are there any specialties that appear in the prescriber table that have no associated prescriptions in the prescription table?
+
+    d. **Difficult Bonus:** *Do not attempt until you have solved all other problems!* For each specialty, report the percentage of total claims by that specialty which are for opioids. Which specialties have a high percentage of opioids?
